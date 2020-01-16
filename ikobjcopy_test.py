9cad21353d1362934dcd0d9d113b18c1242eb619
ikubaku <hide4d51@gmail.com>
2020-01-16 12:55:11 +0900

    Fix format

--- ikobjcopy_test.py
+++ ikobjcopy_test.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 import ikobjcopy
 
 
@@ -9,4 +8,3 @@ Test if the main functioin returns 0
 '''
 def main_test():
     assert ikobjcopy.main() == 0
-    
